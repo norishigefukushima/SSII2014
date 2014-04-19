76c723045e645cfd73b1c746652d9c78ac70d47f
Norishige Fukushima <fukushima@nitech.ac.jp>
2014-04-19 15:53:19 +0900

    update data

--- SSII2014/main.cpp
+++ SSII2014/main.cpp
@@ -1,55 +1,6 @@
 #include "SimdProgrammingLesson.h"
 
-void lesson_1_01_test(float* src, float* dest, int size)
-{
-	for(int i=0;i<size;i++)
-	{
-		dest[i]=src[i];
-	}
-}
-
-void lesson_1_01_answer(float* src, float* dest, int size)
-{
-	int ssesize = size/4;
-	for(int i=0;i<ssesize;i++)
-	{
-		__m128 ms = _mm_load_ps(src);
-		_mm_store_ps(dest, ms);
-		src+=4, dest+=4;
-	}
-}
-
-
-//hint
-//_mm_load_ps
-//_mm_store_ps
-void lesson_1_01(float* src, float* dest, int size)
-{
-	;
-}
-
-
-bool main_lesson_1_01()
-{
-	bool ret=false;
-	const int size = 32;
-	float* src1 = createAlign16Data_32f(size);
-	float* dst1 = createAlign16Data_32f(size);
-	float* dstTest = createAlign16Data_32f(size);
-
-	setLinearData_32f(src1, size);
-
-	lesson_1_01_test(src1,dstTest, size);
-	lesson_1_01(src1, dst1, size);
-	bool result01 = checkData(dst1, dstTest, size);
-	showResult(result01, "Lesson 1-01");
-
-
-	releaseData(src1);
-	releaseData(dst1);
-	releaseData(dstTest);
-	return 0;
-}
+typedef unsigned char uchar ;
 
 //float
 //copy, alloc, set
@@ -72,6 +23,320 @@ bool main_lesson_1_01()
 //image processing?
 int main()
 {
-	main_lesson_1_01();
+	//main_lesson_1_01();
 	return 0;
+}
+
+
+
+void add(uchar* a, uchar* b, uchar* dest, int num)
+{
+	for(int i=0;i<num;i++)
+	{
+		dest[i] = a[i] + b[i];
+	}
+}
+
+void addOpenMP(uchar* a, uchar* b, uchar* dest, int num)
+{
+#pragma omp parallel for
+	for(int i=0;i<num;i++)
+	{
+		dest[i] = a[i] + b[i];
+	}
+}
+
+void add_sse_uchar(uchar* a, uchar* b, uchar* dest, int num)
+{
+	for(int i=0;i<num;i+=16)
+	{
+		//メモリ上の配列A，Bを各をレジスタへロード
+		__m128i ma = _mm_load_si128((const __m128i*)(a+i));
+		__m128i mb = _mm_load_si128((const __m128i*)(b+i));
+		//A,Bが保持されたレジスタの内容を加算してmaのレジスタにコピー
+		ma = _mm_add_epi8(ma,mb);
+		//計算結果のレジスタ内容をメモリ（dest）にストア
+		_mm_store_si128((__m128i*)(dest+i), ma);
+
+	}
+}
+
+
+void add_sse_float(float* a, float* b, float* dest, int num)
+{
+	for(int i=0;i<num;i+=4)
+	{
+		//メモリ上の配列A，Bを各をレジスタへロード
+		__m128 ma = _mm_load_ps((a+i));
+		__m128 mb = _mm_load_ps((b+i));
+		//A,Bが保持されたレジスタの内容を加算してmaのレジスタにコピー
+		ma = _mm_add_ps(ma,mb);
+		//計算結果のレジスタ内容をメモリ（dest）にストア
+		_mm_store_ps((dest+i), ma);
+	}
+}
+
+void add_avx_float(float* a, float* b, float* dest, int num)
+{
+	for(int i=0;i<num;i+=8)
+	{
+		//メモリ上の配列A，Bを各をレジスタへロード
+		__m256 ma = _mm256_load_ps((a+i));
+		__m256 mb = _mm256_load_ps((b+i));
+		//A,Bが保持されたレジスタの内容を加算してmaのレジスタにコピー
+		
+		ma = _mm256_add_ps(ma,mb);
+		//計算結果のレジスタ内容をメモリ（dest）にストア
+		_mm256_store_ps((dest+i), ma);
+	}
+}
+
+float sum(float* src, int num)
+{
+	float ret=0.0f;
+	for(int i=0;i<num;i++)
+	{
+		ret += src[i];
+	}
+	return ret;
+}
+
+float sum2(float* src, int num)
+{
+	float ret0=0.0f;
+	float ret1=0.0f;
+	float ret2=0.0f;
+	float ret3=0.0f;
+
+	for(int i=0;i<num;i+=4)
+	{
+		ret0 += src[4*i+0];
+		ret1 += src[4*i+1];
+		ret2 += src[4*i+2];
+		ret3 += src[4*i+3];
+	}
+
+	return ret0+ret1+ret2+ret3;
+}
+
+float sumOMP(float* src, int num)
+{
+	float ret=0.0f;
+#pragma omp parallel for
+	for(int i=0;i<num;i++)
+	{
+		ret += src[i];
+	}
+	return ret;
+}
+
+float sumOMPTrue(float* src, int num)
+{
+	float ret=0.0f;
+#pragma omp parallel for (+:ret)
+	for(int i=0;i<num;i++)
+	{
+		ret += src[i];
+	}
+	return ret;
+}
+
+void iirfilter(float* src, float* dest, int w, int h, float a)
+{
+	float ia = 1.0f-a;
+	for(int j=1;j<h-1;j++)//画像端を無視
+	{
+		for(int i=1;i<w-1;i++)
+		{
+			float sum = 0.0f;
+			sum+= src[w*(j+l) + i+l];
+			
+			dest[w*j+i] = sum*normalize;
+		}
+	}
+}
+
+void boxfilter(float* src, float* dest, int w, int h, int r)
+{
+	float	normalize = 1.0f/(float)((2*r+1)*(2*r+1));
+	for(int j=r;j<h-r;j++)//画像端を無視
+	{
+		for(int i=r;i<w-r;i++)
+		{
+			float sum = 0.0f;
+			for(int l=-r;l<=r;l++)
+			{
+				for(int k=-r;k<=r;k++)
+				{
+					sum+= src[w*(j+l) + i+l];
+				}
+			}
+			dest[w*j+i] = sum*normalize;
+		}
+	}
+}
+
+
+void boxfilter_omp1(float* src, float* dest, int w, int h, int r)
+{
+	float	normalize = 1.0f/(float)((2*r+1)*(2*r+1));
+#pragma omp parallel for
+	for(int j=r;j<h-r;j++)//画像端を無視
+	{
+		for(int i=r;i<w-r;i++)
+		{
+			float sum = 0.0f;
+			for(int l=-r;l<=r;l++)
+			{
+				for(int k=-r;k<=r;k++)
+				{
+					sum+= src[w*(j+l) + i+l];
+				}
+			}
+			dest[w*j+i] = sum*normalize;
+		}
+	}
+}
+
+void boxfilter_omp2(float* src, float* dest, int w, int h, int r)
+{
+	float	normalize = 1.0f/(float)((2*r+1)*(2*r+1));
+	for(int j=r;j<h-r;j++)//画像端を無視
+	{
+		#pragma omp parallel for
+		for(int i=r;i<w-r;i++)
+		{
+			float sum = 0.0f;
+			for(int l=-r;l<=r;l++)
+			{
+				for(int k=-r;k<=r;k++)
+				{
+					sum+= src[w*(j+l) + i+l];
+				}
+			}
+			dest[w*j+i] = sum*normalize;
+		}
+	}
+}
+
+void boxfilter_omp3(float* src, float* dest, int w, int h, int r)
+{
+	float	normalize = 1.0f/(float)((2*r+1)*(2*r+1));
+
+	for(int j=r;j<h-r;j++)//画像端を無視
+	{
+		for(int i=r;i<w-r;i++)
+		{
+			float sum = 0.0f;
+			#pragma omp parallel for reduction(+:sum)
+			for(int l=-r;l<=r;l++)
+			{
+				for(int k=-r;k<=r;k++)
+				{
+					sum+= src[w*(j+l) + i+l];
+				}
+			}
+			dest[w*j+i] = sum*normalize;
+		}
+	}
+}
+
+
+
+void boxfilter_sse(float* src, float* dest, int w, int h, int r)
+{
+	float	normalize = 1.0f/(float)((2*r+1)*(2*r+1));
+	__m128 mnormalize = _mm_set1_ps(normalize);
+	for(int j=r;j<h-r;j++)//画像端を無視
+	{
+		for(int i=r;i<w-r;i+=4)
+		{
+			__m128 msum = _mm_setzero_ps();
+			for(int l=-r;l<=r;l++)
+			{
+				for(int k=-r;k<=r;k++)
+				{
+					__m128 ms = _mm_loadu_ps((src + w*(j+l) + i+l));
+					msum = _mm_add_ps(msum,ms);
+				}
+			}
+			msum = _mm_mul_ps(msum,mnormalize);
+			_mm_storeu_ps(dest+w*j+i,msum);
+		}
+	}
+}
+
+void boxfilter_sse_omp(float* src, float* dest, int w, int h, int r)
+{
+
+#pragma omp parallel for
+	for(int j=r;j<h-r;j++)//画像端を無視
+	{
+		float	normalize = 1.0f/(float)((2*r+1)*(2*r+1));
+		__m128 mnormalize = _mm_set1_ps(normalize);
+		for(int i=r;i<w-r;i+=4)
+		{
+			__m128 msum = _mm_setzero_ps();
+			for(int l=-r;l<=r;l++)
+			{
+				for(int k=-r;k<=r;k++)
+				{
+					__m128 ms = _mm_loadu_ps((src + w*(j+l) + i+l));
+					msum = _mm_add_ps(msum,ms);
+				}
+			}
+			msum = _mm_mul_ps(msum,mnormalize);
+			_mm_storeu_ps(dest+w*j+i,msum);
+		}
+	}
+}
+
+float sum_sse_float(float* src, int num)
+{
+	__m128 tms = _mm_setzero_ps();
+	
+	for(int i=0;i<num;i+=4)
+	{
+		__m128 ms = _mm_load_ps(src+i);
+		tms = _mm_add_ps(tms,ms);
+	}
+	float data[4];
+	_mm_storeu_ps(data,tms);
+	return (data[0]+data[1]+data[2]+data[3]);
+}
+
+void Gaussianfilter(float* src, float* dest, int w, int h, int r)
+{
+	;
+}
+
+void Sobelfilter(float* src, float* dest, int w, int h, int r)
+{
+	;
+}
+
+void forkjoin_ex(float* src, float* dest0, float* dest1, float* dest2, int w, int h, int r)
+{
+	Gaussianfilter(src,dest0,w,h,r);
+	Sobelfilter(src,dest1,w,h,r);
+	boxfilter(src,dest0,w,h,r);
+}
+
+void forkjoin_ex_omp(float* src, float* dest0, float* dest1, float* dest2, int w, int h, int r)
+{
+#pragma omp parallel sections
+	{
+#pragma omp section
+		{
+			Gaussianfilter(src,dest0,w,h,r);
+		}
+#pragma omp section
+		{
+			Sobelfilter(src,dest1,w,h,r);
+		}
+#pragma omp section
+		{
+			boxfilter(src,dest0,w,h,r);
+		}
+	}
 }
---
+++ SSII2014/sseTest.cpp
@@ -0,0 +1,52 @@
+#include "SimdProgrammingLesson.h"
+
+void lesson_1_01_test(float* src, float* dest, int size)
+{
+	for(int i=0;i<size;i++)
+	{
+		dest[i]=src[i];
+	}
+}
+
+void lesson_1_01_answer(float* src, float* dest, int size)
+{
+	int ssesize = size/4;
+	for(int i=0;i<ssesize;i++)
+	{
+		__m128 ms = _mm_load_ps(src);
+		_mm_store_ps(dest, ms);
+		src+=4, dest+=4;
+	}
+}
+
+
+//hint
+//_mm_load_ps
+//_mm_store_ps
+void lesson_1_01(float* src, float* dest, int size)
+{
+	;
+}
+
+
+bool main_lesson_1_01()
+{
+	bool ret=false;
+	const int size = 32;
+	float* src1 = createAlign16Data_32f(size);
+	float* dst1 = createAlign16Data_32f(size);
+	float* dstTest = createAlign16Data_32f(size);
+
+	setLinearData_32f(src1, size);
+
+	lesson_1_01_test(src1,dstTest, size);
+	lesson_1_01(src1, dst1, size);
+	bool result01 = checkData(dst1, dstTest, size);
+	showResult(result01, "Lesson 1-01");
+
+
+	releaseData(src1);
+	releaseData(dst1);
+	releaseData(dstTest);
+	return 0;
+}
